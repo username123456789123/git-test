e027e255ff84c56f7fe0a9dfb311c9356bbf0a3b
wangbingquan <2455187972@qq.com>
2022-08-21 20:19:24 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("git12");
+        System.out.println("git123456789");
     }
 }
